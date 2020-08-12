4e83252ca6e712303ca835bddf593083a857c88b
Carlos Maldonado <carlosandre@Carloss-Mac-mini.local>
2020-08-12 17:50:24 -0400

    updated deploy link

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://whispering-ravine-93747.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
